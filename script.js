3fe46b95e8cc8b8af41579780006fca501dada1d
wkishore <kwc3@protonmail.com>
2024-05-31 00:46:44 +0530

    Add a basic UI and change JS to accomodate the UI changes

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function userChoice(){
-    let uC = prompt("Rock, Paper, Scissors. Enter your choice:");
-    return uC;
-}
-
+let userScore=0,pcScore=0;
 function pcChoice(){
     let a = Math.random();
     if(a<0.33)
@@ -12,31 +8,47 @@ function pcChoice(){
     return "scissors";
 }
 
-function round(playerFunc, systemFunc){
-    let a = playerFunc();
-    a = a.toLowerCase();
-    let b = systemFunc();
+function round(a){
+    let b = pcChoice();
+    let div = document.querySelector('.anotherOne');
     if(a==b){
-        alert(`System also chose ${b}. Its a tie.`);
-        return 0;
+        div.textContent=`System also chose ${b}. Its a tie.`;
+        return -1;
     }
     if((a=='rock' && b=='paper')||(a=='paper' && b=='scissors')||(a=='scissors' && b=='rock')){
-        alert(`System chose ${b}. System won.`);
+        div.textContent=`System chose ${b}. System won.`;
         return 0;
     }
     else{
-        alert(`System chose ${b}. You won.`);
+        div.textContent=`System chose ${b}. You won.`;
         return 1;
     }
 }
 
 
-function game(){
-    let a=0;
-    for(let i=0;i<5;i++){
-        a+=round(userChoice,pcChoice);
-    }
-    alert(`You won ${a} rounds`);
-}
+let btnParent = document.querySelector('.buttons');
 
-game();
+btnParent.addEventListener('click', function(e){
+    if(userScore>4 || pcScore>4)
+        return;
+    let userChoice = e.target.id;
+    let retval = round(userChoice); 
+    if(retval == -1){
+        userScore++;pcScore++;
+    }
+    else if(retval == 0){pcScore++;}
+    else userScore++;
+
+    let div = document.querySelector('.anotherOne');
+    let scoreDiv = document.querySelector('.text');
+    scoreDiv.textContent = `Player:${userScore} PC:${pcScore}`;
+    if(userScore>4){
+        div.textContent='Player wins. Game Over.';
+        return;
+    }
+    if(pcScore>4){
+        div.textContent='PC wins. Game Over.';
+        return;
+    }
+});
+
